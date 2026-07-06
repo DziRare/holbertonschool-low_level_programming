769eff1e8f426910b72caefb31b69e22d2547cd3
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-06 19:12:45 +1000

    Even more Betty compliant

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+
+/**
+ * main - Entry point of code
+ * Description: Prints a string.
+ * Return: Successful Run
+*/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
