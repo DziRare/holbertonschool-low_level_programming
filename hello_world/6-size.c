3e772ca2c3fc294fbd7440921e7b886c910f7398
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-06 19:53:13 +1000

    Add 6-size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -14,11 +14,11 @@ int main(void)
 	long long my_long_long_int;
 	float my_float;
 
-	printf("Size of a char: %zu (bytes)\n", sizeof(my_char));
-	printf("Size of an int: %zu (bytes)\n", sizeof(my_int));
-	printf("Size of a long int: %zu (bytes)\n", sizeof(my_long_int));
-	printf("Size of a long long int: %zu (bytes)\n", sizeof(my_long_long_int));
-	printf("Size of a float: %zu (bytes)\n", sizeof(my_float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(my_char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(my_int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(my_long_int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(my_long_long_int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(my_float));
 
 	return (0);
 }
