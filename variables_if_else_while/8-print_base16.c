22a3406c2600a2ed8c63721b0fa88b0fd1a5a661
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-07 13:51:00 +1000

    Add 8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of program
+ * Description: Prints all single digit numbers of base 10 starting from zero
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+		putchar(i);
+
+	for (i = 65; i < 71; i++)
+		putchar(i);
+
+	putchar('\n');
+	return (0);
+}
