5a125a2a8386d94289bfc055b59ef7eebaca6678
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-07 10:09:15 +1000

    Fix typo

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -14,11 +14,11 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is postive\n", n);
+		printf("%d is positive\n", n);
 	else if (n < 0)
 		printf("%d is negative\n", n);
 	else
-		printf("%d is 0\n", n);
+		printf("%d is zero\n", n);
 
 	return (0);
 }
