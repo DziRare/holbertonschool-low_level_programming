45e588dd7c654335530799ee54aa6f7d42d4297e
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-18 13:59:15 +1000

    Add 6-puts2.c

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -7,10 +7,14 @@
  */
 void puts2(char *str)
 {
+	int i;
+	i = 0;
+
 	while (*str != '\0')
 	{
-		_putchar(*str);
-		str++;
+		if (i % 2 == 0)
+			_putchar(*str);
+		i++;
 		str++;
 	}
 	_putchar('\n');
