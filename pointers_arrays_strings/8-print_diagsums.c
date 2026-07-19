daeae42be6495f13bf027a8a271e45657a3656f9
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-19 23:54:12 +1000

    Add 8-print_diagsums

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -10,26 +10,17 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
-	int mid;
 	int sum1;
 	int sum2;
 
 	sum1 = 0;
 	sum2 = 0;
-	mid = size / 2;
 	i = 0;
-	while (i <= mid)
+	while (i < size)
 	{
 		sum1 += a[i * size + i];
-		sum1 += a[(size - 1 - i) * size + (size - 1 - i)];
 		sum2 += a[i * size + (size - 1 - i)];
-		sum2 += a[(size - 1 - i) * size + i];
 		i++;
-	}
-	if (size % 2 == 1)
-	{
-		sum1 -= a[mid * size + mid];
-		sum2 -= a[mid * size + mid];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
