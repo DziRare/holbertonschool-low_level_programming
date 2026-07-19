bd5559d7d1a9ebf787bf4760466f6ee96832c018
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-19 13:41:39 +1000

    Add 2-strcpy.c

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncpy - Copy string
+ * @dest: Destination string
+ * @src: Source string
+ * @n: Amount of bytes to be copied
+ * Description: Copies a string to a destination
+ * Return: Pointer to resulting dest string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dest_i;
+	int src_i;
+
+	dest_i = 0;
+	src_i = 0;
+
+	while (src_i < n)
+	{
+		dest[dest_i] = src[src_i];
+		dest_i++;
+		src_i++;
+	}
+
+	return (dest);
+}
