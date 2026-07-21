9aa03aa6794e4d3953c393de50f0d13a5dc88efc
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-21 10:32:27 +1000

    Add malloc_free dir

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - Creates array
+ * @size: Size of array
+ * @c: Character to initialise with
+ * Description: Creates an array of chars, and initializes it with a char
+ * Return: Pointer to array
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *arr;
+	unsigned int i;
+
+	arr = malloc(size * sizeof(char));
+
+	i = 0;
+	while (i < size)
+	{
+		arr[i] = c;
+		i++;
+	}
+	return (arr);
+}
