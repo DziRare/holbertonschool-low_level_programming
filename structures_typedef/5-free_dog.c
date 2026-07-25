e6241be5619384a17bb20d940dca936571a2eba1
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-25 13:42:59 +1000

    Add 5-free_dog.c

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - function to free space
+ *
+ * @d: pointer to dog typedef type
+ * Return: Always success
+ */
+
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
