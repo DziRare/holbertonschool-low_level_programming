791a558fbe46f84c6fe26e946b7742a5868448c2
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-25 13:20:32 +1000

    Add typedef

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,21 +1,23 @@
-#ifndef _DOG_T_H_
-#define _DOG_T_H_
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
- * struct dog_t - Defines a dog
+ * struct dog - Defines a dog
  * @name: Name of dog
  * @age: Age of dog
  * @owner: Owner of dog
  */
-struct dog_t
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
 
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
 
-#endif /* _DOG_T_H_ */
+#endif /* _DOG_H_ */
