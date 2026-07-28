4d649c9f4e51bb23434ca1e61588569b73f7fc37
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-28 11:02:36 +1000

    Add function_pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - Prints a name
+ * @name: Name to be printed
+ * @f: Function that prints name
+ *
+ * Return: No return value
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
