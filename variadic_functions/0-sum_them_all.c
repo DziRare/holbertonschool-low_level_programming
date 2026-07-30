1764dc43488daf5cfe0fb4437303ade6573a3e9e
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-31 09:08:30 +1000

    Betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -11,8 +11,8 @@ int sum_them_all(const unsigned int n, ...)
 	unsigned int i;
 	int total;
 	va_list numbers;
-	va_start(numbers, n);
 
+	va_start(numbers, n);
 	total = 0;
 	for (i = 0; i < n; i++)
 	{
