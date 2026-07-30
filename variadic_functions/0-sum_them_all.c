59d8dff05cecb58249df0b17659f5181d5ac91df
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-31 09:01:18 +1000

    Add variadic_functions folder

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,23 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - Sum of all parameters
+ * @n: First number to be added
+ *
+ * Return: Sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int total;
+	va_list numbers;
+	va_start(numbers, n);
+
+	total = 0;
+	for (i = 0; i < n; i++)
+	{
+		total += va_arg(numbers, int);
+	}
+	va_end(numbers);
+	return (total);
+}
