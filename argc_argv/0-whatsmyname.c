a8412e78355453a257e8c3742b955c1a17383ea2
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-07-31 14:24:06 +1000

    Add 0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+/**
+ * main - Prints the name of program
+ * @argc: Number of command line arguments
+ * @argv: List of command line arguments
+ *
+ * Return: Success (0)
+ */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
