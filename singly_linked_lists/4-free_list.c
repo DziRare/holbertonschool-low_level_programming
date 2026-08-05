df57926248bad291aa2608b5911f3856aaf06691
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-05 11:52:09 +1000

    Add 4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_list - Frees a list
+ * @h: Head of linked list
+ *
+ * Return: No return value
+ */
+void free_list(list_t *head)
+{
+	list_t *current_node;
+	list_t *prev_node;
+
+	current_node = head;
+	while (current_node != NULL)
+	{
+		prev_node = current_node;
+		current_node = current_node->next;
+		free(prev_node);
+	}
+}
