33a31d7fd723f6e5559d133759a51b88c100f4b7
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-05 11:19:49 +1000

    _strlen

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -4,9 +4,29 @@
 #include <stdlib.h>
 
 /**
+ * _strlen - Get the length of a string
+ * @str: The string to be checked
+ *
+ * Return: Number of characters in the string
+ */
+size_t _strlen(const char *str)
+{
+	size_t count;
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count += 1;
+	}
+	return (count);
+}
+
+/**
  * add_node - Adds a new node at the beginning of a linked list
  * @head: Head of a linked list
  * @str: str value of linked list
+ *
+ * Return: Pointer to new node
  */
 list_t *add_node(list_t **head, const char *str)
 {
@@ -23,7 +43,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 	}
 	new->str = strdup(str);
-	new->len = strlen(str);
+	new->len = _strlen(str);
 	new->next = *head;
 	*head = new;
 
