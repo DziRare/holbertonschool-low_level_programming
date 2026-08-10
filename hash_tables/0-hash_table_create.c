b4bbf88b819fa924fc67d367b364ab87c62a709f
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 14:11:09 +1000

    Add arrays in hash table

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -10,13 +10,29 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *hash_table;
+	unsigned long int i;
 
-	hash_table = malloc(sizeof(hash_table_t) * size);
-
+	hash_table = malloc(sizeof(hash_table_t));
 	if (hash_table == NULL)
 	{
 		return (NULL);
 	}
 
-	return hash_table;
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		hash_table->array[i] = NULL;
+		i = i + 1;
+	}
+
+	hash_table->size = size;
+
+	return (hash_table);
 }
