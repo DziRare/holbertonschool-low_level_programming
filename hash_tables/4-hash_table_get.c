bd820344df2ca282146788dbe68a667e9f6db572
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 21:19:38 +1000

    NULL

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -14,6 +14,11 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
 	index = key_index((const unsigned char *)key, ht->size);
 
 	temp = ht->array[index];
