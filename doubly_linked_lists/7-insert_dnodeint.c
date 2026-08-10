3b1966b6bd6c95a97cf959f23a8b36dafe127b05
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 11:51:54 +1000

    Add 7-insert_dnodeint.c

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,54 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_dnodeint_at_index - Inserts new node at given position
+ * @h: Pointer to pointer head of linked list
+ * @idx: Index where node should be added
+ * @n: Value of new node
+ *
+ * Return: Node of nth index or NULL
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int current_index;
+	dlistint_t *tracker;
+	dlistint_t *new_node;
+
+	current_index = 0;
+	tracker = *h;
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	if (*h == NULL)
+		return (NULL);
+
+	while (current_index < idx - 1 && tracker->next)
+	{
+		tracker = tracker->next;
+		current_index = current_index + 1;
+	}
+
+	if (idx == current_index + 1)
+		return (add_dnodeint_end(h, n));
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = tracker;
+	if (tracker->next == NULL)
+	{
+		tracker->next = new_node;
+		new_node->next = NULL;
+	}
+	else
+	{
+		tracker->next->prev = new_node;
+		new_node->next = tracker->next;
+		tracker->next = new_node;
+	}
+
+	return (new_node);
+}
--- doubly_linked_lists/lists.h
+++ doubly_linked_lists/lists.h
@@ -32,5 +32,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 
 int sum_dlistint(dlistint_t *head);
 
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
+
 #endif /* _SAMPLE_HEADER_H_ */
 
