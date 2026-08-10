580343ee14f3bb3dc1ce9a5d42212fc2de17f611
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 16:07:46 +1000

    Add 3-hash_table_set.c

---
+++ hash_tables/3-hash_table_set.c
@@ -0,0 +1,41 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * hash_table_set - Adds an element to hash table
+ * @ht: Pointer to hash table
+ * @key: String that's going to be used as a key for the hash table
+ * @value: Value associated with key
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *new_node;
+
+	index = key_index((const unsigned char *)key, ht->size);
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		return (0);
+	}
+
+	new_node->key = (char *)key;
+	new_node->value = (char *)value;
+	new_node->next = NULL;
+	if (ht->array[index] == NULL)
+	{
+		
+		ht->array[index] = new_node;
+	}
+	else
+	{
+		while (ht->array[index]->next != NULL)
+		{
+			ht->array[index] = ht->array[index]->next;
+		}
+		ht->array[index]->next = new_node;
+	}
+
+	return (1);
+}
