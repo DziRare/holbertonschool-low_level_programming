0f5802779d396e3a451531ba9266d19b2f71c04d
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 11:00:48 +1000

    Add 4-free_dlistint.c

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -6,7 +6,7 @@
  * free_dlistint - Frees a linked list
  * @head: Pointer to head of linked list
  *
- * Return: No return value 
+ * Return: No return value
  */
 void free_dlistint(dlistint_t *head)
 {
