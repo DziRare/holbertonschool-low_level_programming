bc110294990d9f42cadabe8055774bcebcbb708e
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-10 12:01:00 +1000

    Out of range index

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -40,17 +40,9 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	new_node->n = n;
 	new_node->prev = tracker;
-	if (tracker->next == NULL)
-	{
-		tracker->next = new_node;
-		new_node->next = NULL;
-	}
-	else
-	{
-		tracker->next->prev = new_node;
-		new_node->next = tracker->next;
-		tracker->next = new_node;
-	}
+	tracker->next->prev = new_node;
+	new_node->next = tracker->next;
+	tracker->next = new_node;
 
 	return (new_node);
 }
