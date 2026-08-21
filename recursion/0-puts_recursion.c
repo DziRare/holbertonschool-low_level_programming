ce1d926459f47c7ddd8e3838b89aa8f84b3669d3
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-21 10:58:23 +1000

    Add recursion folder

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Print a string followed by a new line
+ */
+void _puts_recursion(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i = i + 1;
+	}
+	_putchar('\n');
+}
---
+++ recursion/main.h
@@ -0,0 +1,8 @@
+#ifndef _RECURSION_H_
+#define _RECURSION_H_
+
+int _putchar(char c);
+
+void _puts_recursion(char *s);
+
+#endif /* _RECURSION_H_ */
