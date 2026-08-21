09885ffd3008e579b45855e08a115f886f10df9b
Munyaradzi Murare <mmurare@yahoo.com.au>
2026-08-21 14:56:06 +1000

    Add 1-print_rev_recursion.c

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Print a string backwards
+ * @s: String to print
+ *
+ * Return: No return value
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		s++;
+		_print_rev_recursion(s);
+		_putchar(*s);
+	}	
+}
